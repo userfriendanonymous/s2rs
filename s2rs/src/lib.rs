79d3788c20c64b024dca795944a512033d1fecb4
userFriend <userfriendanonymous@protonmail.com>
2023-04-24 12:07:43 +0300

    0.2.12

--- s2rs/src/lib.rs
+++ s2rs/src/lib.rs
@@ -3,15 +3,16 @@ pub use api::Api;
 pub use session::Session;
 pub use entities::*; // TO BE ENABLED # DISABLE IS DEV ONLY
 pub use language::Language;
+pub use cursor::Cursor;
 
 pub mod api;
 pub mod session;
 pub mod entities;
+pub mod cursor;
+pub mod language;
 mod utils;
-mod cursor;
 mod cookies;
 mod headers;
-mod language;
 mod json;
 #[cfg(feature = "web_socket")] mod web_socket;
 #[cfg(feature = "html")] mod html;
